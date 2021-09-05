f021e3792eecd2b6fb2a5bda87907b5e29ad6c8f
Damian <damian.groszczyk123@gmail.com>
2021-09-05 12:06:39 +0200

    UserAddTest

--- cypress/integration/app/form.spec.js
+++ cypress/integration/app/form.spec.js
@@ -1,11 +1,24 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
 
-describe('Snapshot test', () => {
+import { forEach } from "lodash";
+
+describe('UserAdd', () => {
     beforeEach(() => {
         cy.visit('localhost:3000');
     })
-    it('Compares snapshot', () => {
-        cy.get('form.form').matchImageSnapshot();
+    it('UserAdd', () => {
+        cy.get(':nth-child(3) > .MuiInputBase-root > .MuiInputBase-input').type("tester")
+        cy.get(':nth-child(4) > .MuiInputBase-root > .MuiInputBase-input').type("tester")
+        cy.get(':nth-child(5) > .MuiInputBase-root > .MuiInputBase-input').type("tester@test.pl")
+        cy.get('form > button').click()
+        cy.visit("http://localhost:3000/users")
+        cy.get('div > h6 + ul > :nth-child(2)').should('be.visible')
+    })
+    it('FalseUserAdd', () => {
+        for (let i = 0; i < 5; i++) {
+            cy.findAllByRole('textbox').eq(i).type("123")
+          }
+          cy.get('form > button').should('be.disabled')
     })
 })
