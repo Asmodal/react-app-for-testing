5e291599fd57cab9a528ea1fe892b3f8fa5e47c7
TesterDamian <88665903+TesterDamian@users.noreply.github.com>
2021-09-05 14:13:36 +0200

    Update src/tests/About.test.js
    Co-authored-by: Radek Miciński <byrm.design@gmail.com>

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,5 +4,5 @@ import { About } from '../pages';
 
 it('First components tests', () => {
   mount(<About/>)
-  cy.get('div#__cy_root > div > h3').contains('About').should('be.visible')
+  cy.get('div > h3').contains('About').should('be.visible')
 });
