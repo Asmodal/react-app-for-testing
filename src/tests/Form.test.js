6c74aac7f209b7dbf85bf21618149d0722e904e7
robertprzybyla94 <robert.przybyla94@gmail.com>
2021-09-05 14:18:48 +0200

    Testy jednostkowe

--- src/tests/Form.test.js
+++ src/tests/Form.test.js
@@ -1,26 +1,12 @@
 /* eslint-disable no-undef */
 import { mount } from '@cypress/react';
-import { Form } from '../components';
+import { About } from '../pages';
 
-const title = 'Title of the form'
+//const title = 'Title of the form'
 
-it('renders learn react link', () => {
-  mount(<Form title={title} />);
-  cy.get('form').contains(title);
-
-});
-it('renders correct number of fields', () => {
-  mount(<Form />);
-  cy.get('form label + div').should('have.length', 6)
-});
-it('renders correct fields', () => {
-  mount(<Form />);
-  const labels = ['name', 'surname', 'email', 'address', 'level', 'description']
-  labels.forEach((label, index) => {
-    cy.get('form label').eq(index).contains(label)
-  })
-});
-it('renders a submit button', () => {
-  mount(<Form />);
-  cy.get('button[type="submit"]').should('exist')
+const title ='About'
+it('check about word', () => {
+  mount(<About />);
+ // cy.get('h3').contains('About')
+ cy.get('h3').contains(title).should('exist')
 });
