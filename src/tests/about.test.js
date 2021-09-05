5cc74f2b5bb5d9582f3ec374393fa948f27abdda
Kamil <54469280+Asmodal@users.noreply.github.com>
2021-09-05 14:16:12 +0200

    test2

--- src/tests/about.test.js
+++ src/tests/about.test.js
@@ -5,8 +5,8 @@ const title = 'About'
 
 it('renders learn react link', () => {
     mount(<About />)
-    cy.get('div > h3').contains('About')
-    cy.get('div > p').contains('This is a single page app for testing purposes only.')
+    cy.get('div > h3').contains('About').should('be.visible')
+    cy.get('div > p').contains('This is a single page app for testing purposes only.').should('be.visible')
   })
 
  
