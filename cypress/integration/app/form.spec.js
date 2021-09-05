a54f4bb610e8b0c3d01685acf0a28295adeaeb1d
Kamil <54469280+Asmodal@users.noreply.github.com>
2021-09-05 11:38:07 +0200

    Logowanie poprawne i Bledne logowanie

--- cypress/integration/app/form.spec.js
+++ cypress/integration/app/form.spec.js
@@ -5,7 +5,28 @@ describe('Snapshot test', () => {
     beforeEach(() => {
         cy.visit('localhost:3000');
     })
-    it('Compares snapshot', () => {
-        cy.get('form.form').matchImageSnapshot();
+    it('Rejestracja użytkownika i sprawdzenie wielkości tabeli', () => {
+        cy.get(':nth-child(3) > .MuiInputBase-root > .MuiInputBase-input').type('Asdads')
+        cy.get(':nth-child(4) > .MuiInputBase-root > .MuiInputBase-input').type('BBBasdf')
+        cy.get(':nth-child(5) > .MuiInputBase-root > .MuiInputBase-input').type('asdfsd@asdfsd.pl')
+        cy.get(':nth-child(6) > .MuiInputBase-root > .MuiInputBase-input').type('safsif')
+        cy.get(':nth-child(7) > .MuiInputBase-root > .MuiInputBase-input').clear().type('4')
+        cy.get(':nth-child(8) > .MuiInputBase-root > .MuiInputBase-input').type('asdfsdfasdasdfasdfsdfsf')
+        cy.get('.MuiButton-label').click()
+        cy.get('[href="/users"]').click()
+        cy.get('ul > li').should('have.length', 3)
+        
     })
+    it('Brak rejestracji i sprawdzenie wielkości tabeli', () => {
+        cy.get(':nth-child(3) > .MuiInputBase-root > .MuiInputBase-input').type('4Asdads')
+        cy.get(':nth-child(4) > .MuiInputBase-root > .MuiInputBase-input').type('5BBBasdf')
+        cy.get(':nth-child(5) > .MuiInputBase-root > .MuiInputBase-input').type('asdfsd@asdfsdpl')
+        cy.get(':nth-child(6) > .MuiInputBase-root > .MuiInputBase-input').type('safsif')
+        cy.get(':nth-child(7) > .MuiInputBase-root > .MuiInputBase-input').clear().type('4')
+        cy.get(':nth-child(8) > .MuiInputBase-root > .MuiInputBase-input').type('asdfsdfasdasdfasdfsdfsf')
+        
+        cy.get('[href="/users"]').click()
+        cy.get('ul > li').should('have.length', 2)
+    })
+
 })
